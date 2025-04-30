c26adb53442830ee1925dfc003c131afb76fb37e
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-04-30 18:45:39 +0900

    🔧 prettierの設定をした

---
+++ .prettierrc.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: 'es5',
+};
+
+export default config;
