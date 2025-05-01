a58175d75a544763612f0683a5a722901e2bfb32
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-05-01 17:48:08 +0900

    💫 単語追加時のフリップアニメーションを実装した

--- src/components/atoms/LetterBox.tsx
+++ src/components/atoms/LetterBox.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { LetterStatus } from '../../type/GameStatus';
 import styles from './LetterBox.module.css';
 
@@ -7,8 +8,37 @@ interface LetterBoxProps {
 }
 
 const LetterBox = ({ letter, status = 'Absent' }: LetterBoxProps) => {
+  const [isFlipping, setIsFlipping] = useState(false);
+  const prevTextRef = useRef('');
+
+  useEffect(() => {
+    if (prevTextRef.current === '' && letter !== '') {
+      setIsFlipping(true);
+
+      const animationTimer = setTimeout(() => {
+        setIsFlipping(false);
+      }, 500);
+
+      return () => {
+        clearTimeout(animationTimer);
+      };
+    }
+
+    prevTextRef.current = letter;
+  }, [letter]);
+
   const statusClass = styles[status.toLocaleLowerCase()];
-  return <div className={`${styles.letterBox} ${statusClass}`}>{letter}</div>;
+
+  return (
+    <div
+      className={`${styles.letterBox} ${isFlipping ? styles.flipActive : ''} ${statusClass}`}
+      style={{
+        visibility: letter !== '' ? 'visible' : 'hidden',
+      }}
+    >
+      {letter}
+    </div>
+  );
 };
 
 export default LetterBox;
--- src/components/molecules/WordRow.tsx
+++ src/components/molecules/WordRow.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { AnswerHistoryLetter } from '../../type/GameStatus';
 import GridCell from '../atoms/GridCell';
 import styles from './WordRow.module.css';
@@ -7,9 +8,35 @@ interface WordRowProps {
 }
 
 const WordRow = ({ histories }: WordRowProps) => {
+  const DELAY_TIME_SECOND = 0.2;
+  const [displayedRow, setDisplayedRow] = useState<AnswerHistoryLetter[]>([]);
+
+  const animatedUpdate = (
+    data: AnswerHistoryLetter[],
+    setState: React.Dispatch<React.SetStateAction<AnswerHistoryLetter[]>>
+  ) => {
+    setState([]);
+    data.forEach((item, index) => {
+      setTimeout(
+        () => {
+          setState((prev) => [...prev, item]);
+        },
+        index * DELAY_TIME_SECOND * 1000
+      );
+    });
+  };
+  useEffect(() => {
+    if (
+      histories.length > 0 &&
+      histories[0].letter !== displayedRow[0]?.letter
+    ) {
+      animatedUpdate(histories, setDisplayedRow);
+    }
+  }, [histories]);
+
   return (
     <div className={styles.wordRow}>
-      {histories.map((history, index) => (
+      {displayedRow.map((history, index) => (
         <GridCell key={index} letter={history.letter} status={history.status} />
       ))}
     </div>
